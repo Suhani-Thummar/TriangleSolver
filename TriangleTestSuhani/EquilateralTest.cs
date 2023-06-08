39e86da5702c8ececc79e9278e673a978c9c4dd7
Suhani Thummar <suhanithummar2041@gmail.com>
2023-06-08 14:07:49 -0400

    Add project files.

---
+++ TriangleTestSuhani/EquilateralTest.cs
@@ -0,0 +1,29 @@
+﻿using TriangleSolver;
+using NUnit.Framework;
+using System;
+
+
+namespace TriangleTestSuhani
+
+{
+    [TestFixture]
+    public class EquilateralTest
+    {
+        [Test]
+        public void TestClass()
+        {
+            //arrange
+            int firstSide = 60;
+            int secondSide = 60;
+            int thirdSide = 60;
+            string expected = "The triangle is valid and is an EQUILATERAL";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
