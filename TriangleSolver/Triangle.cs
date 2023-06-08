f4cd2a414a0274eb428faac77a44764bff9893f9
Suhani Thummar <suhanithummar2041@gmail.com>
2023-06-08 15:01:09 -0400

    Test number 3 of SCALENE Triangle is done.

--- TriangleSolver/Triangle.cs
+++ TriangleSolver/Triangle.cs
@@ -42,5 +42,6 @@ namespace TriangleSolver
             
             return output;
         }
+
     }
 }
--- TriangleTestSuhani/TriangleTestCases.cs
+++ TriangleTestSuhani/TriangleTestCases.cs
@@ -85,6 +85,104 @@ namespace TriangleTestSuhani
 
         }
 
-    }
+        // test-3.A ---- SCALENE test case 1 is perfromed ----
+        public void ValideTringle_InputTest1_ForScalane()
+        {
+            //Arragne
+            int firstSide = 70;
+            int secondSide = 60;
+            int thirdSide = 50;
 
+
+            string expected = "The triangle is valid and is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+        // test-3.B ---- SCALENE test case 2 is perfromed ----
+        public void ValideTringle_InputTest2_ForScalane()
+        {
+            //Arragne
+            int firstSide = 60;
+            int secondSide = 65;
+            int thirdSide = 55;
+
+
+            string expected = "The triangle is valid and is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+        // test-3.C ---- SCALENE test case 3 is perfromed ----
+        public void ValideTringle_InputTest3_ForScalane()
+        {
+            //Arragne
+            int firstSide = 75;
+            int secondSide = 45;
+            int thirdSide = 60;
+
+
+            string expected = "The triangle is valid and is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+        // test-3.D ---- SCALENE test case 4 is perfromed ----
+        public void ValideTringle_InputTest4_ForScalane()
+        {
+            //Arragne
+            int firstSide = 80;
+            int secondSide = 40;
+            int thirdSide = 60;
+
+
+            string expected = "The triangle is valid and is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+        // test-3.E ---- SCALENE test case 5 is perfromed ----
+        public void ValideTringle_InputTest5_ForScalane()
+        {
+            //Arragne
+            int firstSide = 40;
+            int secondSide = 55;
+            int thirdSide = 85;
+
+
+            string expected = "The triangle is valid and is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+    }
 }
